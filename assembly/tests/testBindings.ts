27f696549114782415e1003a0a39b905d630bf48
JairusSW <jairus.v.tanaka@outlook.com>
2021-07-14 16:13:35 -0700

    push

--- assembly/tests/testBindings.ts
+++ assembly/tests/testBindings.ts
@@ -1,7 +1,23 @@
 import { console } from "../index";
 
-console.log('Outside')
-
 export function test(): void {
-    console.log('Inside')
+    console.assert(false, 'assert')
+    console.count('count')
+    console.countReset('count')
+    console.debug('debug')
+    console.error('error')
+    console.group('group')
+    console.group('group')
+    console.group('group')
+    console.groupCollapsed('groupCollapsed')
+    console.groupCollapsed('groupCollapsed')
+    console.groupCollapsed('groupCollapsed')
+    console.groupEnd()
+    console.info('info')
+    console.log('log')
+    console.time('time')
+    console.timeLog('time')
+    console.timeEnd('time')
+    console.trace('trace')
+    console.warn('warn')
 }
